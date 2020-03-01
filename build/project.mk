8fa6330eb5ed7397a135dd57ea3f2891f7012ab0
Amos Gheva <ghevaamos@gmail.com>
2020-02-29 22:56:46 -0500

    importing my build system

---
+++ build/project.mk
@@ -0,0 +1,67 @@
+include $(strip ${BLDFILES})/functions.mk
+
+ifndef DEBUG_MAKE
+QQ := @
+endif
+
+ifdef DEBUG
+CXXFLAGS += -g
+endif
+
+SUBDIRS :=
+STATIC_MODULES :=
+SHARED_MODULES :=
+SHARED_LIBS :=
+EXES :=
+OBJS :=
+LIBDIR := $(strip ${OUTDIR})/$(strip ${LIBDIR})
+EXEDIR := $(strip ${OUTDIR})/$(strip ${EXEDIR})
+INCLUDIR := $(strip ${OUTDIR})/$(strip ${INCLUDIR})
+CREATE_DIRS := ${OUTDIR} ${LIBDIR} ${EXEDIR} ${INCLUDIR}
+HEADERS :=
+
+CXXFLAGS += -I${SRCDIR}
+LDFLAGS += -L${LIBDIR}
+
+.PHONY: all
+all: buil_all
+
+# Start from the source directory and traverse down
+include $(strip ${SRCDIR})/build.mk
+
+# Add all the subdirectories
+$(foreach sd, ${SUBDIRS}, $(eval $(call ADD_SUBDIR, ${sd})))
+
+# Collect all objects into dependencies
+$(foreach h, ${HEADERS}, $(eval $(call HEADER_DEP, ${h})))
+
+$(foreach m, ${STATIC_MODULES}, $(eval $(call STATIC_LIB_DEP, ${m})))
+
+$(foreach m, ${SHARED_MODULES}, $(eval $(call SHARED_LIB_DEP, ${m})))
+
+$(foreach e, ${EXECUTABLES}, $(eval $(call EXE_DEP, ${e})))
+
+.PHONY: buil_all
+buil_all: ${CREATE_DIRS} ${HEADER_DEPS} ${STATIC_LIBS} ${SHARED_LIBS} ${EXES} ${CREATE_DIRS}
+
+clean:
+	${QQ}${RMDIR} ${OUTDIR}
+	${QQ}if [ -n "${UNITTESTS_MAIN}" ] ; then ${RM_F} ${UNITTESTS_MAIN}; fi
+
+$(foreach d, $(sort ${CREATE_DIRS}), $(eval $(call DIRDEP, ${d})))
+
+$(foreach m, ${STATIC_MODULES}, $(eval $(call STATIC_LIB_TARGET, ${m})))
+
+$(foreach m, ${SHARED_MODULES}, $(eval $(call SHARED_LIB_TARGET, ${m})))
+$(foreach m, ${EXECUTABLES}, $(eval $(call EXE_TARGET, ${m})))
+
+# Handle dependencies
+-include $(OBJS:.o=.d)
+
+include $(strip ${BLDFILES})/targets.mk
+
+${UNITTESTS_MAIN}: ${UNITTESTS_SUITES}
+	${QQ}$(strip ${BLDFILES})/utils/unittests_main.awk $^ > $@
+
+# vim: set noexpandtab :
+
---
+++ build/targets.mk
@@ -0,0 +1,31 @@
+# Targets for specific build artefacts 
+
+# Build C++ into an object, store it's dependencies
+${OUTDIR}/%.$(strip ${OBJ_SFX}):${SRCDIR}%.cpp
+	$(call MESSAGE, Building $@)
+	${QQ}${CXX} ${CXXFLAGS} ${CFLAGS} ${$(strip $@)_FLAGS} -fPIC -c -o $@ $<
+	@${CXX} ${CXXFLAGS} ${CFLAGS} ${$(strip $@)_FLAGS} -MM $< | sed 's;^[^:]*:;$@:;' > ${OUTDIR}/$*.d
+
+${OUTDIR}/%.$(strip ${OBJ_SFX}):${SRCDIR}%.c
+	$(call MESSAGE, Building $@)
+	${QQ}${CC} ${CFLAGS} ${$(strip $@)_FLAGS} -fPIC -c -o $@ $<
+	@${CC} ${CFLAGS} ${$(strip $@)_FLAGS} -MM $< | sed 's;^[^:]*:;$@:;' > ${OUTDIR}/$*.d
+
+
+# Link shared libraries
+%.$(strip ${SHARED_SFX}):
+	$(call MESSAGE, Linking shared $@)
+	${QQ}$(CXX) $(CXXFLAGS) $(CFLAGS) $(LDFLAGS) -shared -o $@ ${ADD_ARCHIVE_FLAG} $^ ${ARCHIVES_ADDED_FLAG} $($(strip $(basename $(notdir $@)))_FLAGS)
+
+# Link static libraries
+%.$(strip ${STATIC_SFX}):
+	$(call MESSAGE, Linking static $@)
+	${QQ}ar cr $@ $^
+
+# Copy exported headers
+$(strip ${INCLUDIR})/%.h:${SRCDIR}%.h
+	$(call MESSAGE, Copying header $@)
+	${QQ}cp $< $@
+
+# vim: set noexpandtab :
+
