1ba5a76390cb5580e45f3f5a42825e4e1f611078
Amos Gheva <ghevaamos@gmail.com>
2020-03-26 12:33:44 -0400

    adding some sample code

--- src/apps/wip.cpp
+++ src/apps/wip.cpp
@@ -8,6 +8,54 @@
 using namespace sap;
 using namespace std;
 
+void do_fft(WAVFile& wav)
+{
+  float* contents;
+  wav.read_fully(&contents);
+  fftwf_plan plan_;
+  fftwf_complex* in_;
+  fftwf_complex* out_;
+  int size = 4400;
+  in_ = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * size);
+  out_ = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * size);
+  plan_ = fftwf_plan_dft_1d(size, in_, out_, FFTW_FORWARD, FFTW_MEASURE);
+  for (int i = 0; i < size; ++i)
+  {
+      in_[i][0] = contents[i];
+      in_[i][1] = 0;
+  }
+  fftwf_execute(plan_);
+
+  fftwf_destroy_plan(plan_);
+  fftwf_free(in_);
+  fftwf_free(out_);
+  fftwf_cleanup();
+}
+
+void do_fft2(WAVFile& wav)
+{
+  float* contents;
+  wav.read_fully(&contents);
+  fftwf_plan plan_;
+  float* in_;
+  fftwf_complex* out_;
+  int size = 400;
+  MultiTaper tapers(size);
+  in_ = (float*)fftwf_malloc(sizeof(float) * size);
+  out_ = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * size);
+  plan_ = fftwf_plan_dft_r2c_1d(size, in_, out_, FFTW_MEASURE);
+  for (int i = 0; i < size; ++i)
+  {
+      in_[i] = contents[i] * tapers[0][i];
+  }
+  fftwf_execute(plan_);
+
+  fftwf_destroy_plan(plan_);
+  fftwf_free(in_);
+  fftwf_free(out_);
+  fftwf_cleanup();
+}
+
 int main(int argc, char** argv)
 {
   if (argc != 2)
@@ -19,7 +67,9 @@ int main(int argc, char** argv)
   //int slices = total/44.1;
 
   int window_size = 44;
+  do_fft2(wav);
 
+  //*
   MultiTaper tapers(window_size);
   Fft fft1(window_size);
   Fft fft2(window_size);
@@ -32,6 +82,7 @@ int main(int argc, char** argv)
   {
     cerr << "success" << endl;
   }
+  // */
 
   //initYin(sample_rate, 40);//option->minFreq);
 
