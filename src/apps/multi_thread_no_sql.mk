fd714e8d4525b465690cbc1ca51c39907359d715
Amos Gheva <ghevaamos@gmail.com>
2020-04-17 11:59:35 -0400

    allowing for different linkages

--- src/apps/multi_thread_no_sql.mk
+++ src/apps/multi_thread_no_sql.mk
@@ -10,7 +10,9 @@ multi_thread_no_sql_MODULES += yin
 multi_thread_no_sql_MODULES += tapers
 multi_thread_no_sql_FLAGS += -L$(strip ${THIRD_PARTY_BUILD})/fftw/lib 
 multi_thread_no_sql_FLAGS += -lfftw3f
-#multi_thread_no_sql_FLAGS += -lfftw3f_threads
+ifndef WINDOWS
+multi_thread_no_sql_FLAGS += -lfftw3f_threads
+endif
 
 # vim: set noexpandtab :
 
